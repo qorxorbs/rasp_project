68ee093cc8e4bcac433a847a17dbf62a752d9ff0
qorxorbs77 <qorxorbs77@naver.com>
2023-01-03 15:34:38 +0900

    파이썬 코드 학습

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,22 @@
 print("hello")
 
+list_a=[1,2,3,4,5]
+list_b=[6,7,8,9,10]
+list_c=["aa","bb","cc","dd","ee"]
 
+print(list_a[0])
+print(list_b[1])
+print(list_c[2])
+
+k=input("알파벳 입력하세요")
+if(k=="a"):
+    print("a가 정상 입력됨")
+else:
+    print("다른 키가 입력됨")
+
+m=input("숫자 입력하세요")
+m=int(m)
+if(m>=10):
+    print("10보다 크거나 같다")
+else:
+    print("10보다 작다")
